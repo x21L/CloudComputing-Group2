06ae7e186e52fb3ac37f811531169652dc9b4d69
christoph <christoph.burghuber@email.com>
2021-01-04 20:09:25 +0100

    Added allow all cors header.

--- microservice-search/webapi/BookStoreSearch/BookStoreSearch/Startup.cs
+++ microservice-search/webapi/BookStoreSearch/BookStoreSearch/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BookStoreSearch.Contract;
 using BookStoreSearch.Entity;
 using BookStoreSearch.Impl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -19,6 +14,18 @@ namespace BookStoreSearch
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
             services.AddControllers();
             services.AddScoped<ISearchService<Book>, ElasticSearchServiceImpl<Book>>();
         }
@@ -26,6 +33,8 @@ namespace BookStoreSearch
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowAll");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
