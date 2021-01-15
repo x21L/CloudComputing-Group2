d37a9258fcb8d638e301ba4755555eec3cc5c1d9
MaxU11 <49079692+MaxU11@users.noreply.github.com>
2021-01-15 10:34:09 +0100

    v5

--- frontend-bookstore/bookstore/src/components/App.js
+++ frontend-bookstore/bookstore/src/components/App.js
@@ -59,7 +59,7 @@ const App = () => {
      <div className="App">
       <Header text="Book-Store" />
       <Search search={search} />
-      <p className="App-intro">V4 Searchresults for: {searchValueString}</p>
+      <p className="App-intro">V5 Searchresults for: {searchValueString}</p>
       <div className="Book">
         {loading && !errorMessage ? (
          <span>loading...</span>
