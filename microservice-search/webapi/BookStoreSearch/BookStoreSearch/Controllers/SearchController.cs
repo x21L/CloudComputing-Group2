f46b21f9b1bb8c5155b6e9e16d36a4603fb4d7cf
christoph <christoph.burghuber@email.com>
2021-01-15 17:42:32 +0100

    Changed implementation.

--- microservice-search/webapi/BookStoreSearch/BookStoreSearch/Controllers/SearchController.cs
+++ microservice-search/webapi/BookStoreSearch/BookStoreSearch/Controllers/SearchController.cs
@@ -29,8 +29,40 @@ namespace BookStoreSearch.Controllers
         [HttpGet]
         public async Task<ActionResult> Get([FromQuery(Name = "query")] string query, [FromQuery(Name = "from")] int from = 0, [FromQuery(Name = "size")] int size = 10)
         {
-            await Task.Run(() => true);
-            return Ok("Hello");
+            if (query.Length > 1000)
+            {
+                throw new ArgumentException("Query cannot be longer than 1000 characters!");
+            }
+
+            if (from < 0)
+            {
+                throw new ArgumentException("From has to be bigger than 0!");
+            }
+
+            if (size < 0)
+            {
+                throw new ArgumentException("Size has to be bigger than 0!");
+            }
+
+            if (size > 100)
+            {
+                throw new ArgumentException("Size has to be smaller or equal 100!");
+            }
+
+            var settings = new SearchSettings
+            {
+                From = from,
+                Size = size
+            };
+
+            var result = await _searchService.Search(query, settings);
+
+            if (result.Any())
+            {
+                return Ok(result);
+            }
+
+            return StatusCode((int)HttpStatusCode.NoContent);
         }
     }
 }
