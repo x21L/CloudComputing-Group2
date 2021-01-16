c495939f64eec88954afe587359dc5d887034438
MaxU11 <49079692+MaxU11@users.noreply.github.com>
2021-01-16 21:43:05 +0100

    shop

--- frontend-bookstore/bookstore/src/components/Book.js
+++ frontend-bookstore/bookstore/src/components/Book.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-const DEFAULT_PLACEHOLDER_IMAGE =
-  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+const Book = (props) => {
 
+  const buyBook = () => {
+    props.add(props.book.id);
+  }
 
-const Book = ({ book }) => {
   return (
     <div className="book">
-      <h2>{book.name}</h2>
+      <h2>{props.book.name}</h2>
       <div>
         <img
           width="100"
-          alt={`Title: ${book.name}`}
+          alt={`Title: ${props.book.name}`}
           src={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Closed_Book_Icon.svg/1200px-Closed_Book_Icon.svg.png"}
         />
       </div>
-      <p>Description: {book.description}</p>
-      <p>Author: {book.author}</p>
+      <p>Description: {props.book.description}</p>
+      <p>Author: {props.book.author}</p>
+      <p><button id="buyBook" onClick={buyBook}></button></p>
       <hr/>
     </div>
   );
