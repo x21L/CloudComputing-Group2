d17669e3b877c9f13d642e2cf901e9921f986cc9
Lukas Wais <lukas.wais@jku.at>
2021-01-17 08:38:28 +0100

    updated test message

--- shopping-cart/src/main/java/group02/lukaswais/shopping_cart/controller/Controller.java
+++ shopping-cart/src/main/java/group02/lukaswais/shopping_cart/controller/Controller.java
@@ -2,6 +2,9 @@ package group02.lukaswais.shopping_cart.controller;
 
 import com.google.gson.Gson;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * This list fills the list with books and executes the SQL queries.
  * It also provides the information for the servlet.
@@ -27,7 +30,8 @@ public class Controller {
         return new Gson().toJson(databaseController.getUser(userID));
     }
 
-    public String test() throws ClassNotFoundException {
-        return new Gson().toJson("test from the controller " + databaseController.getConnection() + " " + Class.forName("com.mysql.jdbc.Driver"));
+    public String test() throws ClassNotFoundException, SQLException {
+        return new Gson().toJson("test from the controller " + databaseController.getConnection() + " " + Class.forName("com.mysql.jdbc.Driver") +
+                " " + DriverManager.getConnection("jdbc:mysql://10.8.11.20:3306", "root", "password"));
     }
 }
